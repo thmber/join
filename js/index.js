902bb385c9294f19f6c8972115cf4f6ca4aa4f0c
TM <thmahler@gmx.net>
2023-04-01 15:52:01 +0200

    added arrow

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 function showSignUp(){
     let loginBox = document.getElementById('log-in-content');
     loginBox.innerHTML = `
+        <span onclick="showLogin()" class="arrow-left">&larr;</span>
         <span class="login-headline" id="login-headline">Sign Up</span>
         <div class="underline-login" id="login-underline">
         </div>
@@ -28,6 +29,7 @@ function showSignUp(){
 function showForgotPassword(){
     let loginBox = document.getElementById('log-in-content');
     loginBox.innerHTML = `
+        <span onclick="showLogin()" class="arrow-left">&larr;</span>
         <span class="login-headline" id="login-headline">I forgot my password</span>
         <div class="underline-login" id="login-underline">
         </div>
@@ -45,4 +47,41 @@ function showForgotPassword(){
             </div>
         </form>
     `;
+}
+
+
+function showLogin(){
+    let loginBox = document.getElementById('log-in-content');
+    loginBox.innerHTML = `
+                <div class="sign-box" id="sign-box">
+                <span class="login-headline" id="login-headline">Log In</span>
+                <div class="underline-login" id="login-underline">
+                </div>
+                <form onsubmit="login()" class="login-form">
+                    <div class="login-box">
+                        <input type="email" required minlength="5" placeholder="Email">
+                        <img src="icon-email.svg" alt="">
+                    </div>
+                    <div class="login-box">
+                        <input type="password" required minlength="5" placeholder="Password">
+                        <img src="icon-password.svg" alt="">
+                    </div>
+                    <div class="remember-forgot-box">
+                        <div class="remember-check">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">Remember Me</label>
+                        </div>
+                        <a onclick="showForgotPassword()">Forgot my password</a>
+                    </div>
+                    <div class="login-and-guest">
+                        <button class="login">Log in</button>
+                        <a class="guest-login" href="summary.html">Guest Log in</a>
+                    </div>
+                </form>
+            </div>
+            <div class="forgot-container d-none" id="forgot-container">
+                
+            </div>
+    `;
+
 }
